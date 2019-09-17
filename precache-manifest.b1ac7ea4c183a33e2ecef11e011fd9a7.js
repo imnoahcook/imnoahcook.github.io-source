8c36bfcb4b5985f11a27fa7397fc6c6cbf3d65b2
Cooklord <imnoahcook@gmail.com>
2019-09-17 15:36:49 -0400

    Updates

---
+++ precache-manifest.b1ac7ea4c183a33e2ecef11e011fd9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92afd81c628ff49871ecdffd5bc19197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4882ae4059cde5f0b481",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3f3eb88956bda50cea3a",
+    "url": "/static/css/main.9b7579e8.chunk.css"
+  },
+  {
+    "revision": "4882ae4059cde5f0b481",
+    "url": "/static/js/2.d25e5b7c.chunk.js"
+  },
+  {
+    "revision": "3f3eb88956bda50cea3a",
+    "url": "/static/js/main.c6196d54.chunk.js"
+  },
+  {
+    "revision": "0f22f7e697ec619ebcd1",
+    "url": "/static/js/runtime~main.a82acdc9.js"
+  }
+]);
